831905d0b81306c6c72f215704fee6d454835e6a
kaola526 <huweixiong@storswift.com>
2024-04-08 14:32:20 +0800

    add test

--- node/src/prover/parse.rs
+++ node/src/prover/parse.rs
@@ -137,20 +137,27 @@ pub async fn send_post_request(
         task_id, task_type, json_proof, zk_type, name_space
     );
 
-    println!("POST data {}", json_data);
+    println!("POST: {url}\n data {}", json_data);
 
     let response = client
         .post(url)
         .header("Content-Type", "application/json")
         .body(json_data.to_owned())
         .send()
-        .await?;
+        .await;
 
-    // 检查响应状态码
-    if response.status().is_success() {
-        println!("POST请求成功！");
-    } else {
-        println!("POST请求失败：{}", response.status());
+    match response {
+        Ok(response) => {
+            if response.status().is_success() {
+                println!("POST请求成功！");
+            } else {
+                println!("POST请求失败：{}", response.status());
+            }
+        },
+        Err(error) => {
+            println!("POST error: {}", error);
+            return Err(error);
+        }
     }
 
     Ok(())
@@ -158,7 +165,7 @@ pub async fn send_post_request(
 
 #[cfg(test)]
 mod tests {
-    use crate::prover::parse::InputArgs;
+    use crate::prover::parse::{send_post_request, InputArgs};
 
     #[test]
     fn test_parse_development_and_genesis() {
@@ -173,13 +180,14 @@ mod tests {
     #[tokio::test]
     async fn test_send_post_request() {
         let url = "https://127.0.0.1:9084/api/v1/computing/cp/receive/ubi";
-        let task_id = "10011";
-        let task_type = "1";
-        let proof = "id:10011;nonce:2882303761517117440;solution:puzzle1cryng056kcer4duyaxu68uc3w6hsxsajs0wrwu0xepgsqffsx6rm5pk3t0m9a2yq63pnsr7jw64qz9zdwf6";
+        let task_id = 10011;
+        let task_type = 1;
+        let nonce = 2882303761517117440u64;
+        let solution = "puzzle1cryng056kcer4duyaxu68uc3w6hsxsajs0wrwu0xepgsqffsx6rm5pk3t0m9a2yq63pnsr7jw64qz9zdwf6";
         let zk_type = "aleo-proof";
         let name_space = "ubi-task-10011";
 
-        let result = send_post_request(url, task_id, task_type, proof, zk_type, name_space).await;
+        let result = send_post_request(url, task_id, task_type, nonce, solution, 100, zk_type, name_space).await;
 
         assert!(result.is_ok());
     }
